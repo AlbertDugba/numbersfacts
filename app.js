f6cbfcaaf567505ce081ee7e1fc83e94c0a00597
ALbertDugba <albert.dugba@gmail.com>
2018-07-10 11:57:40 +0000

    Added Numbers APi

---
+++ app.js
@@ -0,0 +1,21 @@
+let factText = document.querySelector('#factsText')
+let fact = document.querySelector('#fact')
+let hide = document.querySelector('#hide')
+let numberInput = document.querySelector('#numInput')
+numberInput.addEventListener('input', getFactAJAX)
+
+function getFactAJAX(){
+    let number = numberInput.value;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://numbersapi.com/'+ number);  
+    xhr.onload = function(){
+        if(this.status == 200 && number != ''){
+            hide.style.display = 'block';
+            factText.innerText = this.responseText;
+            console.log(this.responseText)
+        }
+
+    }
+    // console.log(number)
+    xhr.send();
+}
